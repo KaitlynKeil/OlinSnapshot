e9ae6c489a204a8fac69999ea5607168a3c86716
Frackleton <mack.frackleton@gmail.com>
2018-03-05 16:22:38 -0500

    clean out superfluous code

--- app/routes.py
+++ app/routes.py
@@ -3,6 +3,7 @@ from app import app
 
 @app.route('/')
 @app.route('/index')
+@app.route('/data')
 def index():
     user = {'username': 'Olin'}
     return render_template('index.html', title='Home', user=user)
