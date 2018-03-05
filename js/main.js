db17731194f98c3113be6605ae6a39982d111cb5
Wilson Tang <wilson.t.tang@gmail.com>
2018-03-05 12:00:27 -0500

    last front end commit

--- js/main.js
+++ js/main.js
@@ -99,81 +99,326 @@ var data_sample =
   ]
 };
 
-//Create the SVG body
-var svg = d3.select("body")
-    .append("svg")
-    .attr("width", '800px')
-    .attr("height", '800px');
+var data_sample_2 = 
+{
+  "category":"Lost",
+  "value": 20,
+  "emails": [
+    {
+      "event_time": "17:22:57",
+      "body": "I am putting a Party",
+      "event_place": "Boston Commons",
+      "subject": "Boston Tea Party",
+      "event_data": "2018-03-02",
+      "value": 5,
+      "who": "kaitlyn"
+    },
+    {       
+      "event_time": "17:22:57",
+      "body": "I am putting a Party",
+      "event_place": "Boston Commons",
+      "subject": "Boston Tea Party",
+      "event_data": "2018-03-02",
+      "value": 5,
+      "who": "kaitlyn"
+    },
+    {       
+      "event_time": "17:22:57",
+      "body": "I am putting a Party",
+      "event_place": "Boston Commons",
+      "subject": "Boston Tea Party",
+      "event_data": "2018-03-02",
+      "value": 5,
+      "who": "kaitlyn"
+    },
+    {       
+      "event_time": "17:22:57",
+      "body": "I am putting a Party",
+      "event_place": "Boston Commons",
+      "subject": "Boston Tea Party",
+      "event_data": "2018-03-02",
+      "value": 5,
+      "who": "kaitlyn"
+    }
+  ]
+};
 
-//Create all the circle objects based on the data
-svg.selectAll("circle")
-    .data([32, 57, 112, 293]) //bind data
-  .enter().append("circle") //create circles 
-    .attr("cy", 60) //y axis
-    .attr("cx", function(d, i) { return i * 100 + 30; }) //x axis
-    .attr("r", function(d) { return Math.sqrt(d); }); //radius
+var data_sample_3 =
 
-//create an empty tool top
-var tooltip = d3.select("body")
-  .append ("div")
-  .style("position", "absolute")
-  .style("z-index", "10")
-  .style("visibility", "hidden")
-  .style("color", "white")
-  .style("padding", "8px")
-  .style("background-color", "rgba(0, 0, 0, 0.75)")
-  .style("border-radius", "6px")
-  .style("font", "12px sans-serif")
-  .text("tooltip");
-//
+{ "emails":[
+    {
+      "event_time": "17:22:57",
+      "body": "I am putting a Party",
+      "event_place": "Boston Commons",
+      "subject": "Boston Tea Party",
+      "event_data": "2018-03-02",
+      "value": 5,
+      "who": "kaitlyn"
+    },
+    {       
+      "event_time": "17:22:57",
+      "body": "I am putting a Party",
+      "event_place": "Boston Commons",
+      "subject": "Boston Tea Party",
+      "event_data": "2018-03-02",
+      "value": 5,
+      "who": "kaitlyn"
+    },
+    {       
+      "event_time": "17:22:57",
+      "body": "I am putting a Party",
+      "event_place": "Boston Commons",
+      "subject": "Boston Tea Party",
+      "event_data": "2018-03-02",
+      "value": 5,
+      "who": "kaitlyn"
+    },
+    {       
+      "event_time": "17:22:57",
+      "body": "I am putting a Party",
+      "event_place": "Boston Commons",
+      "subject": "Boston Tea Party",
+      "event_data": "2018-03-02",
+      "value": 5,
+      "who": "kaitlyn"
+    }
+  ]
+};
+
+
+
+var test =
+{
+  "name": "Data Development",
+  "value": 60,
+  "children": [
+    {"name": "D3.js", "value": 10},
+    {"name": "Dimple.js", "value": 8},
+    {"name": "matplotlib(python)", "value": 6},
+    {"name": "ggplot(R)", "value": 4},
+    {"name": "numpy", "value": 9},
+    {"name": "scikit-learn", "value": 8}
+  ]
+};
+
+
+var test_2 =
+{
+  "name": "Lost",
+  "value": 60,
+  "helpme": [
+    {
+      "event_time": "17:22:57",
+      "body": "I am putting a Party",
+      "event_place": "Boston Commons",
+      "subject": "Boston Tea Party",
+      "event_data": "2018-03-02",
+      "value": 5,
+      "who": "kaitlyn"
+    },
+    {       
+      "event_time": "17:22:57",
+      "body": "I am putting a Party",
+      "event_place": "Boston Commons",
+      "subject": "Boston Tea Party",
+      "event_data": "2018-03-02",
+      "value": 5,
+      "who": "kaitlyn"
+    },
+    {       
+      "event_time": "17:22:57",
+      "body": "I am putting a Party",
+      "event_place": "Boston Commons",
+      "subject": "Boston Tea Party",
+      "event_data": "2018-03-02",
+      "value": 5,
+      "who": "kaitlyn"
+    },
+    {       
+      "event_time": "17:22:57",
+      "body": "I am putting a Party",
+      "event_place": "Boston Commons",
+      "subject": "Boston Tea Party",
+      "event_data": "2018-03-02",
+      "value": 5,
+      "who": "kaitlyn"
+    }
+
+  ]
+};
+
+var test_3 =
+{
+  "name": "Lost",
+  "value": 60,
+  "children": [
+    {
+      "event_time": "17:22:57",
+      "body": "I am putting a Party",
+      "event_place": "Boston Commons",
+      "name": "Boston Tea Party",
+      "event_data": "2018-03-02",
+      "value": 5,
+      "who": "kaitlyn"
+    },
+    {       
+      "event_time": "17:22:57",
+      "body": "I am putting a Party",
+      "event_place": "Boston Commons",
+      "name": "Boston Tea Party",
+      "event_data": "2018-03-02",
+      "value": 5,
+      "who": "kaitlyn"
+    },
+    {       
+      "event_time": "17:22:57",
+      "body": "I am putting a Party",
+      "event_place": "Boston Commons",
+      "name": "Boston Tea Party",
+      "event_data": "2018-03-02",
+      "value": 5,
+      "who": "kaitlyn"
+    },
+    {       
+      "event_time": "17:22:57",
+      "body": "I am putting a Party",
+      "event_place": "Boston Commons",
+      "name": "Boston Tea Party",
+      "event_data": "2018-03-02",
+      "value": 5,
+      "who": "kaitlyn"
+    }
+  ]
+};
+
+/*var color = d3.scale.category20()
+
+var svgContainer = d3.select("body").append("svg")
+  .attr("width", 200)
+  .attr("height", 200);
+
+var circles = svgContainer.selectAll("circle")
+  .data(data_sample.data)
+  .enter()
+  .append("circle");
+
+var circleAttributes = circles
+  .attr("cx", 100)
+  .attr("cy", 100)
+  .attr("r", 50)//function (d) { return d.data.category.emails.length })
+  .style("fill", color);
+
+circles.append("text")
+      .text(function(d) { return d.emails.subject; });*/
+
+
+
+var width = 800, height = 600;
+
+var chart = d3.select("body").append("svg")
+  .attr("width", width).attr("height", height)
+  .append("g")
+    .attr("transform", "translate(50,50)");
+
+var pack = d3.layout.pack()
+  .size([width, height - 50])
+  .padding(10);
+
+var nodes = pack.nodes(test_2);
+
+var node = chart.selectAll(".node")
+    .data(nodes).enter()
+  .append("g")
+    .attr("class", "node")
+    .attr("transform", function(d) { return "translate(" + d.x + "," + d.y + ")"; });
+
+node.append("circle")
+    .attr("r",function(d) { return d.r; })
+    .attr("fill", function(d){ return d.helpme ? "#fff" : "steelblue"; }) //make nodes with children invisible
+    .attr("opacity", 0.25)
+    .attr("stroke", function(d) { return d.helpme ? "#fff":"#ADADAD"; } ) //make nodes with children invisible
+    .attr("stroke-width", 2);
+
+node.append("text")
+    .text(function(d) { return d.helpme? "" : d.who; });
+
+
+/*var width = 800, height = 600;
+
+var chart = d3.select("body").append("svg")
+  .attr("width", width).attr("height", height)
+  .append("g")
+    .attr("transform", "translate(50,50)");
+
+var pack = d3.layout.pack()
+  .size([width, height - 50])
+  .padding(10);
+
+
+var nodes = pack.nodes(data_sample_2);
+
+var node = chart.selectAll(".node")
+    .data(nodes).enter()
+  .append("g")
+    .attr("class", "node")
+    .attr("transform", function(d) { return "translate(" + d.value + "," + d.value + ")"; });
+
+node.append("circle")
+    .attr("r",function(d) { return d.value;})//d.emails.length * 30; })
+    .attr("fill", function(d){ return d.emails ? "#fff": "steelblue";}) //make nodes with children invisible
+    .attr("opacity", 0.25)
+    .attr("stroke", function(d) { return  d.emails ? "#fff": "#ADADAD"; } ) //make nodes with children invisible
+    .attr("stroke-width", 2);
+
+node.append("text")
+    .text(function(d) { return d.emails ? "" : d.subject });*/
+
 /*
-createGraph();
+var width = 800, height = 600;
 
-function createGraph() {*/
-    var width = 960; // chart width
-    var height = 700; // chart height
-    var format = d3.format(",d");  // convert value to integer
-    //v3 syntax
-    var color = d3.scale.category20();  // create ordinal scale with 20 colors
-    var sizeOfRadius = d3.scale.pow().domain([-100,100]).range([-50,50]);  // https://github.com/mbostock/d3/wiki/Quantitative-Scales#pow
+var chart = d3.select("body").append("svg")
+  .attr("width", width).attr("height", height)
+  .append("g")
+    .attr("transform", "translate(50,50)");
 
-    //create bubble in a specific layout format
-    var bubble = d3.layout.pack()
-        .sort(null)  // disable sorting, use DOM tree traversal
-        .size([width, height])  // chart layout size
-        .padding(1)  // padding between circles
-        .radius(function(d) { return 20 }) //;+ (sizeOfRadius(d.data.category.emails.length) * 30); });  // radius for each circle
-
-    //initialize SVG Body/chart
-    var svg = d3.select('#chart').append('svg') //add the bubbles to all charts
-        .attr('width',width)
-        .attr('height',height)
-        .attr('class', 'bubble');
-
-    //"LOAD" DATA
-    var nodes = bubble.nodes(data_sample)
-      
-    //create a node for each bubble file
-    var node = svg.selectAll(".node")
-      //bind data 
-      .data(nodes).enter()
-    .append("g")
-      .attr("class", "node")
-      .attr("transform", function(d) { return "translate(" + d.x + "," + d.y + ")"; });
-
-    //add attributes to each create bubble
-    node.append("circle")
-      .attr("r",function(d) { return d.data.category.emails.length; })
-      .attr("fill", color) //make nodes with children invisible
-      .attr("opacity", 0.25)
-      .attr("stroke", color) //make nodes with children invisible
-      .attr("stroke-width", 2);
-
-    //add labeled text on top of each bubble
-    node.append("text")
-      .text(function(d) { return d.name; });
+var pack = d3.layout.pack()
+  .size([width, height - 50])
+  .padding(10);
 
 
+var nodes = pack.nodes(data_sample_3);
+
+var node = chart.selectAll(".node")
+    .data(nodes).enter()
+  .append("g")
+    .attr("class", "node")
+    .attr("transform", function(d) { return "translate(" + 50 + "," + 50 + ")"; });
+
+node.append("circle")
+    .attr("r",function(d) { return 50;})//d.emails.length * 30; })
+    .attr("fill", function(d){ return  "steelblue";}) //make nodes with children invisible
+    .attr("opacity", 0.25)
+    .attr("stroke", function(d) { return "#ADADAD"; } ) //make nodes with children invisible
+    .attr("stroke-width", 2);
+
+node.append("text")
+    .text(function(d) { return d.emails ? "" : d.subject });
+*/
+//var test = function(d) {return d.emails; };
+//console.log(data_sample_2.emails.subject);
+//console.log(d3.select("body").append("svg").attr("width", width).attr("height", height).selectAll(".node").data(nodes).enter().append("g"));
+/*
+node.append("text")
+    .text("balls");*/
+
+
+
+
+
+
+
+
+//console.log(d3.select("body").append("svg").attr("width", 200).attr("height", 200).selectAll("circle").data(data_sample.data).enter().append("circle"));
     
     //Load data, issue with referencing data.json while in local http server
 /*    d3.json("/data/data.json", function(data) {
