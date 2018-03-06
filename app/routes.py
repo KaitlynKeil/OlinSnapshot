7eef59127ee179664197fb3e16cf087f5d2fa3f6
KaitlynKeil <kaitly.keil@students.olin.edu>
2018-03-06 14:29:02 -0500

    Added the scraper, hopefully it works!

--- app/routes.py
+++ app/routes.py
@@ -8,10 +8,14 @@ def index():
     return render_template('index.html', title='Home', user=user)
 
 from app import postgres_parser as pp # anything that is imported from this file needs to be app.<name>
+from app import email_scraper as es
 
 @app.route("/data") # set the name of the page it is writing to
 def data():
     conn, cur = pp.connect() # connect to the database and return the connection and cursor
+    email_list = es.get_mail()
+    for email in email_list:
+    	add_email(cur, email)
     json_string = pp.all_cats_to_json(conn) # make a json string from it
-    pp.no_commit_close_conn(conn, cur) # close the connection, but don't change things
+    pp.close_conn(conn, cur) # close the connection, but don't change things
     return jsonify(json_string) # return the json, which will be accessible from the url/data
