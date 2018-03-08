4542ba674ec01d4ba6e7b9306381384842929137
Wilson Tang <wilson.t.tang@gmail.com>
2018-03-08 15:45:11 -0500

    trying different structure to allow for updates

--- app/static/js/main.js
+++ app/static/js/main.js
@@ -5,6 +5,7 @@ $(function() {
   createGraph();
 });
 
+
 function createGraph() {
   //set screen pixel sizes
   var width = 800, height = 600;
@@ -22,8 +23,35 @@ function createGraph() {
 
   //Requesting the data bound to /data endpoint
   d3.json("/data", function(error,quotes) {
-    
-    //create category json 
+    input_data =  
+      {
+        "name": "Categories",
+        "value": 60,
+        "children": [
+          {
+            "name":  "Food",
+            "value": quotes.data[0].children.length * 5,
+            "color": "green"
+          },
+          {
+            "name":  "Other",
+            "value": quotes.data[1].children.length * 5,
+            "color": "purple"
+          },
+          {
+            "name":  "Lost",
+            "value": quotes.data[2].children.length * 5,
+            "color": "red"
+          },
+          {
+            "name":  "Events",
+            "value": quotes.data[3].children.length * 5,
+            "color": "blue"
+          }
+        ]
+      };
+    updateGraphics(input_data,quotes);
+/*    //create category json 
     var categories = {
     "name": "Categories",
     "value": 60,
@@ -114,6 +142,7 @@ function createGraph() {
     node.exit().remove();
 
     console.log("we tried!")
+  */
   });
 };
 
@@ -169,7 +198,7 @@ function updateData(selected_category) {
   }
 };
 
-/*function updateGraphics(input_data){
+function updateGraphics(input_data, quotes){
     //select all node and join new data in
     var node = chart.selectAll(".node")
       .data(input_data).enter()
@@ -196,14 +225,42 @@ function updateData(selected_category) {
     //add click functionality
     node.on('click',datum => {
       console.log(datum);
-      updateData(datum.name);
-    })
+      switch(datum.name){
+        case "Food":
+          var nodes = pack.nodes(quotes.data[0]);
+          updateGraphics(nodes,quotes);
+          console.log("Food selected");
+          console.log(nodes);
+          break;
+        case "Other":
+          var nodes = pack.nodes(quotes.data[1]);
+          console.log("Other selected");
+          updateGraphics(nodes,quotes);
+          console.log(nodes);
+          break;
+        case "Events":
+          var nodes = pack.nodes(quotes.data[2]);
+          updateGraphics(nodes,quotes);
+          console.log("Events selected");
+          console.log(nodes);
+          break;
+        case "Lost":
+          var nodes = pack.nodes(quotes.data[3]);
+          updateGraphics(nodes,quotes);
+          console.log("Lost selected");
+          console.log(nodes);
+          break;
+        default:
+          console.log("Category not found!") 
+
+      }
+    });
 
     //remove old elements
     node.exit().remove();
 
     console.log("we tried!")
-};*/
+};
 
 
 
